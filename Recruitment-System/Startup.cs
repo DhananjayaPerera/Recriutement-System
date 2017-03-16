169046249a179c2f9038c4c1692bae13a4e96d7f
Mohamed Afrar <mmafrar@gmail.com>
2017-03-17 03:27:45 +0530

    Added the basic structure for the project

---
+++ Recruitment-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recruitment_System.Startup))]
+namespace Recruitment_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
